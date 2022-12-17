57e77ab0db9561cc852ca62c2703244fc0e6ffd5
Nikola <veljanovskin92@gmail.com>
2022-12-17 20:55:42 +0100

    init

---
+++ Natega/NategaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NategaApp.swift
+//  Natega
+//
+//  Created by Nikola Veljanovski on 17.12.22.
+//
+
+import SwiftUI
+
+@main
+struct NategaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
